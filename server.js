3e9724a55ca4bced2f5623eca5149a202ab8af46
vikeshpoojary <vrp6000@gmail.com>
2016-09-22 21:30:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/articleone', function(req,res){
-    res.send('Article ONE');
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/articletwo', function(req,res){
     res.send('Article TWO');
